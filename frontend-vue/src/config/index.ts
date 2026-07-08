cf3e6254cd5f2ec221e91f2ba7ec95e35143497d
陈雨钒 <2923095084@qq.com>
2026-07-08 14:48:22 +0800

    chore: rename project to 软竞云

--- frontend-vue/src/config/index.ts
+++ frontend-vue/src/config/index.ts
@@ -38,7 +38,7 @@ import { headerBarConfig } from './modules/headerBar'
 const appConfig: SystemConfig = {
   // 系统信息
   systemInfo: {
-    name: 'SSGL 竞赛管理' // 系统名称
+    name: '软竞云' // 系统名称
   },
   // 系统主题
   systemThemeStyles: {
